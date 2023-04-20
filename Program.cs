1fc8e0375dde9a3812d9ec57b014d5fa71f1b28a
Jonatandb <jonatandb@gmail.com>
2023-04-20 09:32:37 -0300

    Configuration for PostgreSQL added

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Added configuration for PostgreSQL
+var configuration = builder.Configuration;
+builder.Services.AddDbContext<UserContext>(options =>
+    options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
